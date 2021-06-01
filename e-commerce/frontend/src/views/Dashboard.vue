34918bdbef2f0a6fe585d41bd55028cefc7cf922
Fernando Gomez <shadyfast8.services@gmail.com>
2021-06-01 20:01:20 +0200

    Add dashboard styling for categories buttons

--- e-commerce/frontend/src/views/Dashboard.vue
+++ e-commerce/frontend/src/views/Dashboard.vue
@@ -6,74 +6,88 @@
     srcset=""
     class="logo">
     <h2>Select a category to start looking products</h2>
-    <h3 class="subtitle">Leftology</h3>
-    <hr class="line">
-    <router-link
-    class="dashboard__leftology category"
-    :to="{ name: 'Search', params: { category: 'leftology' } }"
-    >
-      <img
-      class="category__image"
-      src="https://i.ibb.co/6gxLC4J/tecnologia-zurdo.jpg"
-      alt="leftology"
-      srcset=""
-      >
-    </router-link>
-    <h3 class="subtitle">Lefticians</h3>
-    <hr class="line">
-    <router-link
-    class="dashboard__lefticians category"
-    :to="{ name: 'Search', params: { category: 'lefticians' } }"
-    >
-      <img
-      class="category__image"
-      src="https://i.ibb.co/yqLTYf4/instrumentos-zurdo.webp"
-      alt="lefticians"
-      srcset=""
-      >
-    </router-link>
 
-    <h3 class="subtitle">Office</h3>
-    <hr class="line">
-    <router-link
-    class="dashboard__office category"
-    :to="{ name: 'Search', params: { category: 'office' } }"
-    >
-      <img
-      class="category__image"
-      src="https://i.ibb.co/wNvQCsv/oficina-zurdo.jpg"
-      alt="office"
-      srcset=""
-      >
-    </router-link>
+    <div class="categories">
+      <span>
+        <h3 class="subtitle">Leftology</h3>
+        <hr class="line">
+        <router-link
+        class="dashboard__leftology category"
+        :to="{ name: 'Search', params: { category: 'leftology' } }"
+        >
+          <img
+          class="category__image"
+          src="https://i.ibb.co/6gxLC4J/tecnologia-zurdo.jpg"
+          alt="leftology"
+          srcset=""
+          >
+        </router-link>
+      </span>
 
-    <h3 class="subtitle">Kitchen</h3>
-    <hr class="line">
-    <router-link
-    class="dashboard__kitchen"
-    :to="{ name: 'Search', params: { category: 'kitchen' } }"
-    >
-      <img
-      class="category__image"
-      src="https://i.ibb.co/5cTDgD9/cocina-zurdo.jpg"
-      alt="kitchen"
-      srcset=""
-      >
-    </router-link>
+      <span>
+        <h3 class="subtitle">Lefticians</h3>
+        <hr class="line">
+        <router-link
+        class="dashboard__lefticians category"
+        :to="{ name: 'Search', params: { category: 'lefticians' } }"
+        >
+          <img
+          class="category__image"
+          src="https://i.ibb.co/yqLTYf4/instrumentos-zurdo.webp"
+          alt="lefticians"
+          srcset=""
+          >
+        </router-link>
+      </span>
 
-    <h3 class="subtitle">Clothes</h3>
-    <hr class="line">
-    <router-link
-    class="dashboard__clothes"
-    :to="{ name: 'Search', params: { category: 'clothes' } }"
-    >
-      <img
-      class="category__image"
-      src="https://i.ibb.co/ccbDDTZ/ropa-zurdo.jpg"
-      alt="clothes"
-      srcset=""
-      >
-    </router-link>
+      <span>
+        <h3 class="subtitle">Office</h3>
+        <hr class="line">
+        <router-link
+        class="dashboard__office category"
+        :to="{ name: 'Search', params: { category: 'office' } }"
+        >
+          <img
+          class="category__image"
+          src="https://i.ibb.co/wNvQCsv/oficina-zurdo.jpg"
+          alt="office"
+          srcset=""
+          >
+        </router-link>
+      </span>
+
+      <span>
+        <h3 class="subtitle">Kitchen</h3>
+        <hr class="line">
+        <router-link
+        class="dashboard__kitchen"
+        :to="{ name: 'Search', params: { category: 'kitchen' } }"
+        >
+          <img
+          class="category__image"
+          src="https://i.ibb.co/5cTDgD9/cocina-zurdo.jpg"
+          alt="kitchen"
+          srcset=""
+          >
+        </router-link>
+      </span>
+
+      <span>
+        <h3 class="subtitle">Clothes</h3>
+        <hr class="line">
+        <router-link
+        class="dashboard__clothes"
+        :to="{ name: 'Search', params: { category: 'clothes' } }"
+        >
+          <img
+          class="category__image"
+          src="https://i.ibb.co/ccbDDTZ/ropa-zurdo.jpg"
+          alt="clothes"
+          srcset=""
+          >
+        </router-link>
+      </span>
+    </div>
   </div>
 
 </template>
@@ -101,34 +115,18 @@ h2 {
   padding: 1em 0em;
 }
 
-h3 {
-  padding: 0.2em;
-  font-size: 2em;
-}
-
-hr {
-  width: 80%;
-  background-color: #966BDD;
-  border: none;
-  height: 1px;
-  margin-bottom: 1em;
-}
-
 .dashboard{
-  padding-top: 8em;
+  padding-top: 9em;
   display: flex;
   flex-direction: column;
   align-items: center;
 
-  &__leftology {
-
-    }
-
   img {
     height: 12em;
     width: 20em;
+    object-fit: cover;
     border-radius: 1em 1em 0em 1em;
-    box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
+    box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);
     transition: all 0.3s cubic-bezier(.25,.8,.25,1);
   }
 
@@ -139,13 +137,39 @@ hr {
   .logo{
     display: flex;
     align-self: center;
-    height: 8em;
-    width: 15em;
+    height: 100%;
+    width: 19em;
     box-shadow: none;
   }
 
   .logo:hover {
     box-shadow: none;
+  }
+}
+
+.categories {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+
+  span {
+    padding: 0em 1em 1em 1em;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  h3 {
+    padding: 0.2em;
+    font-size: 2em;
+  }
+
+  hr {
+    width: 80%;
+    background-color: #966BDD;
+    border: none;
+    height: 1px;
+    margin-bottom: 1em;
   }
 }
 
