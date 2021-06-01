226c8d59f671bae542e20588adc1eee7cf5367ab
Fernando Gomez <shadyfast8.services@gmail.com>
2021-06-01 12:11:33 +0200

    Create header component and attach to App

--- e-commerce/frontend/src/App.vue
+++ e-commerce/frontend/src/App.vue
@@ -1,16 +1,23 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> |
-    <router-link to="/profile">Profile</router-link> |
-    <router-link to="/wishlist">Wishlist</router-link> |
-    <router-link to="/cart">Cart</router-link> |
-    <router-link to="/dashboard">Dashboard</router-link> |
-    <router-link to="/detail">Detail</router-link> |
-    <router-link to="/search">Search</router-link> |
-  </div>
+  <Header />
   <router-view/>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Header from './components/Header.vue';
+
+export default defineComponent({
+  components: {
+    Header,
+  },
+  data() {
+    return {
+    };
+  },
+  methods: {},
+});
+</script>
 
 <style lang="scss">
 
---
+++ e-commerce/frontend/src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link> |
+    <router-link to="/profile">Profile</router-link> |
+    <router-link to="/wishlist">Wishlist</router-link> |
+    <router-link to="/cart">Cart</router-link> |
+    <router-link to="/dashboard">Dashboard</router-link> |
+    <router-link to="/detail">Detail</router-link> |
+    <router-link to="/search">Search</router-link> |
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Header',
+  components: {
+  },
+  setup() {
+    return {};
+  },
+});
+</script>
