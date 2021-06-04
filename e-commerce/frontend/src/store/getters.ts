af3be7e8f09d968c0dc2a661f77ed3df7ac333af
Fernando Gomez <shadyfast8.services@gmail.com>
2021-06-04 17:10:36 +0200

    Delete unneeded getters

--- e-commerce/frontend/src/store/getters.ts
+++ e-commerce/frontend/src/store/getters.ts
@@ -2,13 +2,6 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 const getters = {
-  getCount(state:any) {
-    return state.count;
-  },
-
-  getProducts(state:any) {
-    return state.products;
-  },
 
   getProductsByCategory:
   (state:any) => (category: any) => state.products
@@ -22,10 +15,6 @@ const getters = {
     return state.cart.length;
   },
 
-  getCurrentProduct(state:any) {
-    return state.currentProduct;
-  },
-
   getCurrentProductRate(state:any) {
     const ratingNumbers:any = [];
     state.currentProduct.ratings.forEach((rate:any) => { ratingNumbers.push(rate.rating); });
@@ -36,10 +25,6 @@ const getters = {
   (state:any) => (id: any) => state.products.find((product:any) => product._id === id),
   getCart(state:any) {
     return state.cart;
-  },
-
-  getWishlist(state:any) {
-    return state.wishlist;
   },
 
   isInWishlist:
