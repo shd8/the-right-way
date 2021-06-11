d8726191074fa73a2a84e017dca5ea5c76157d73
Fernando Gomez <shadyfast8.services@gmail.com>
2021-06-11 21:16:40 +0200

    Add header testing

--- e-commerce/frontend/tests/unit/header.spec.ts
+++ e-commerce/frontend/tests/unit/header.spec.ts
@@ -8,10 +8,84 @@ test('displays message', () => {
       plugins: [router],
       mocks: {
         $store: {
-          getters: {
-            getCartLength: '1',
-            getWishlistLength: '1',
+          state: {
+            wishlist: ['abcd123'],
+            cart: ['abcd123', 'abcd321'],
+            products: [
+              {
+                _id: 'abcd123',
+                name: 'product1',
+                category: 'LEFTICIANS',
+                price: 5,
+                stock: 0,
+                images: [],
+                ratings: [
+                  {
+                    user: 'one',
+                    rating: 1,
+                    comment: 'comment one',
+                  },
+                  {
+                    user: 'two',
+                    rating: 2,
+                    comment: 'comment two',
+                  },
+                ],
+              },
+              {
+                _id: 'abcd321',
+                name: 'product2',
+                category: 'OFFICE',
+                price: 0,
+                stock: 0,
+                images: [],
+                ratings: [],
+              },
+            ],
+            currentProduct: {
+              _id: '',
+              name: 'productA',
+              category: '',
+              price: 0,
+              stock: 0,
+              images: [],
+              ratings: [{
+                user: 'one',
+                rating: 1,
+                comment: 'comment one',
+              },
+              {
+                user: 'two',
+                rating: 2,
+                comment: 'comment two',
+              },
+              ],
+            },
+            currentUser: {
+              _id: '',
+              username: '',
+              email: '',
+              password: '',
+              address: {
+                country: '',
+                city: '',
+                street: '',
+                postalCode: '',
+              },
+            },
+            isUserLogged: false,
+            token: '',
+            rightMode: false,
           },
+          getters: {
+            isInCart: () => true,
+            getCartLength: () => 1,
+            getWishlistLength: () => 1,
+          },
+        },
+        data: {
+          showModal: false,
+          searchInput: 'a',
         },
       },
     },
