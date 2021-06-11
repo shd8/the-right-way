831708b2fde543a93d60bb473774b300d01d040b
Fernando Gomez <shadyfast8.services@gmail.com>
2021-06-11 20:22:25 +0200

    Add category selector test

---
+++ e-commerce/frontend/tests/unit/categorySelector.spec.ts
@@ -0,0 +1,108 @@
+import { mount } from '@vue/test-utils';
+import CategorySelector from '@/components/CartProduct.vue';
+import router from '@/router/index';
+
+describe('Given a category selector', () => {
+  test('click on first routes to ./clothes', async () => {
+    const $route = {
+      path: '/search/clothes',
+    };
+
+    const wrapper = mount(CategorySelector, {
+      props: {
+        id: '1',
+        name: 'Product1',
+        price: 3,
+        images: [],
+      },
+      global: {
+        plugins: [router],
+        mocks: {
+          router,
+          $route,
+          $router: {
+            push: jest.fn(),
+          },
+          $store: {
+            state: {
+              wishlist: ['abcd123'],
+              cart: ['abcd123', 'abcd321'],
+              products: [
+                {
+                  _id: 'abcd123',
+                  name: 'product1',
+                  category: 'LEFTICIANS',
+                  price: 5,
+                  stock: 0,
+                  images: [],
+                  ratings: [
+                    {
+                      user: 'one',
+                      rating: 1,
+                      comment: 'comment one',
+                    },
+                    {
+                      user: 'two',
+                      rating: 2,
+                      comment: 'comment two',
+                    },
+                  ],
+                },
+                {
+                  _id: 'abcd321',
+                  name: 'product2',
+                  category: 'OFFICE',
+                  price: 0,
+                  stock: 0,
+                  images: [],
+                  ratings: [],
+                },
+              ],
+              currentProduct: {
+                _id: '',
+                name: 'productA',
+                category: '',
+                price: 0,
+                stock: 0,
+                images: [],
+                ratings: [{
+                  user: 'one',
+                  rating: 1,
+                  comment: 'comment one',
+                },
+                {
+                  user: 'two',
+                  rating: 2,
+                  comment: 'comment two',
+                },
+                ],
+              },
+              currentUser: {
+                _id: '',
+                username: '',
+                email: '',
+                password: '',
+                address: {
+                  country: '',
+                  city: '',
+                  street: '',
+                  postalCode: '',
+                },
+              },
+              isUserLogged: false,
+              token: '',
+              rightMode: false,
+            },
+            getters: {
+              isInCart: () => true,
+            },
+          },
+        },
+      },
+    });
+
+    // await wrapper.find('.clothes').trigger('click');
+
+    await expect(wrapper.text()).toContain('');
+  });
+});
