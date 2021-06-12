e671ec70f3e86acc956eea65561201d91951d3cb
Fernando Gomez <shadyfast8.services@gmail.com>
2021-06-12 23:07:58 +0200

    Test login component up to 75%

--- e-commerce/frontend/tests/unit/loginRegister.spec.ts
+++ e-commerce/frontend/tests/unit/loginRegister.spec.ts
@@ -1,10 +1,102 @@
 import { mount } from '@vue/test-utils';
-import Login from '@/components/Login.vue';
-import router from '@/router/index';
+import LoginRegister from '@/components/LoginRegister.vue';
 
-describe('Given a login component', () => {
-  test('Mount a Login component', () => {
-    const wrapper = mount(Login, {
+describe('Given a loginregister component', () => {
+  test('Mount a LoginRegister component', () => {
+    const wrapper = mount(LoginRegister, {
+      global: {
+        mocks: {
+          methods: {
+            handleToggleModal: jest.fn(),
+          },
+          $store: {
+            state: {
+              wishlist: ['abcd123'],
+              cart: ['abcd123', 'abcd321'],
+              products: [
+                {
+                  _id: 'abcd123',
+                  name: 'product1',
+                  category: 'LEFTICIANS',
+                  price: 5,
+                  stock: 0,
+                  images: [],
+                  ratings: [
+                    {
+                      user: 'one',
+                      rating: 1,
+                      comment: 'comment one',
+                    },
+                    {
+                      user: 'two',
+                      rating: 2,
+                      comment: 'comment two',
+                    },
+                  ],
+                },
+                {
+                  _id: 'abcd321',
+                  name: 'product2',
+                  category: 'OFFICE',
+                  price: 0,
+                  stock: 0,
+                  images: [],
+                  ratings: [],
+                },
+              ],
+              currentProduct: {
+                _id: '',
+                name: 'productA',
+                category: '',
+                price: 0,
+                stock: 0,
+                images: [],
+                ratings: [{
+                  user: 'one',
+                  rating: 1,
+                  comment: 'comment one',
+                },
+                {
+                  user: 'two',
+                  rating: 2,
+                  comment: 'comment two',
+                },
+                ],
+              },
+              currentUser: {
+                _id: '',
+                username: '',
+                email: '',
+                password: '',
+                address: {
+                  country: '',
+                  city: '',
+                  street: '',
+                  postalCode: '',
+                },
+              },
+              isUserLogged: true,
+              token: '',
+              rightMode: false,
+            },
+            getters: {
+              getCurrentProductRate: 5,
+              isInWishlist: true,
+            },
+            actions: {
+              fetchProductsFromApi: {},
+            },
+          },
+        },
+      },
+    });
+
+    // Assert the rendered text of the component
+    expect(wrapper.text()).toContain('');
+  });
+
+  test('Mount a LoginRegister component', () => {
+    const wrapper = mount(LoginRegister, {
       global: {
         mocks: {
           methods: {
@@ -90,102 +182,9 @@ describe('Given a login component', () => {
           },
         },
       },
-      props: {
-        id: 2,
-      },
     });
 
     // Assert the rendered text of the component
     expect(wrapper.text()).toContain('Login or create an account to buy stuff, save your Wishlist and your conversations !');
-  });
-
-  test('Mount a Login component', () => {
-    const wrapper = mount(Login, {
-      global: {
-        plugins: [router],
-        mocks: {
-          $store: {
-            state: {
-              wishlist: ['abcd123'],
-              cart: ['abcd123', 'abcd321'],
-              products: [
-                {
-                  _id: 'abcd123',
-                  name: 'product1',
-                  category: 'LEFTICIANS',
-                  price: 5,
-                  stock: 0,
-                  images: [],
-                  ratings: [
-                    {
-                      user: 'one',
-                      rating: 1,
-                      comment: 'comment one',
-                    },
-                    {
-                      user: 'two',
-                      rating: 2,
-                      comment: 'comment two',
-                    },
-                  ],
-                },
-                {
-                  _id: 'abcd321',
-                  name: 'product2',
-                  category: 'OFFICE',
-                  price: 0,
-                  stock: 0,
-                  images: [],
-                  ratings: [],
-                },
-              ],
-              currentProduct: {
-                _id: '',
-                name: 'productA',
-                category: '',
-                price: 0,
-                stock: 0,
-                images: [],
-                ratings: [{
-                  user: 'one',
-                  rating: 1,
-                  comment: 'comment one',
-                },
-                {
-                  user: 'two',
-                  rating: 2,
-                  comment: 'comment two',
-                },
-                ],
-              },
-              currentUser: {
-                _id: '',
-                username: '',
-                email: '',
-                password: '',
-                address: {
-                  country: '',
-                  city: '',
-                  street: '',
-                  postalCode: '',
-                },
-              },
-              isUserLogged: true,
-              token: '',
-              rightMode: false,
-            },
-            getters: {
-              isUserLogged: () => true,
-            },
-            actions: {
-              logUserRequest: () => 1,
-            },
-          },
-        },
-      },
-    });
-
-    // Assert the rendered text of the component
-    expect(wrapper.text()).toContain('');
   });
 });
