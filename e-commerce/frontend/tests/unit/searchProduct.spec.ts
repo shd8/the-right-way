74dbe756c4f3cd0d4e14c355272fbe7c8f974a57
Fernando Gomez <shadyfast8.services@gmail.com>
2021-06-12 23:21:53 +0200

    Test search product component up to 100%

---
+++ e-commerce/frontend/tests/unit/searchProduct.spec.ts
@@ -0,0 +1,98 @@
+import { mount } from '@vue/test-utils';
+import SearchProduct from '@/components/SearchProduct.vue';
+import router from '@/router/index';
+
+describe('Given an Cart Product component', () => {
+  test('Should mount Cart Product', () => {
+    const wrapper = mount(SearchProduct, {
+      props: {
+        id: '1',
+        name: 'Product1',
+        price: 3,
+        images: [],
+      },
+      global: {
+        plugins: [router],
+        mocks: {
+          $store: {
+            state: {
+              wishlist: ['abcd123'],
+              cart: ['abcd123', 'abcd321'],
+              products: [
+                {
+                  _id: 'abcd123',
+                  name: 'product1',
+                  category: 'LEFTICIANS',
+                  price: 5,
+                  stock: 0,
+                  images: [],
+                  ratings: [
+                    {
+                      user: 'one',
+                      rating: 1,
+                      comment: 'comment one',
+                    },
+                    {
+                      user: 'two',
+                      rating: 2,
+                      comment: 'comment two',
+                    },
+                  ],
+                },
+                {
+                  _id: 'abcd321',
+                  name: 'product2',
+                  category: 'OFFICE',
+                  price: 0,
+                  stock: 0,
+                  images: [],
+                  ratings: [],
+                },
+              ],
+              currentProduct: {
+                _id: '',
+                name: 'productA',
+                category: '',
+                price: 0,
+                stock: 0,
+                images: [],
+                ratings: [{
+                  user: 'one',
+                  rating: 1,
+                  comment: 'comment one',
+                },
+                {
+                  user: 'two',
+                  rating: 2,
+                  comment: 'comment two',
+                },
+                ],
+              },
+              currentUser: {
+                _id: '',
+                username: '',
+                email: '',
+                password: '',
+                address: {
+                  country: '',
+                  city: '',
+                  street: '',
+                  postalCode: '',
+                },
+              },
+              isUserLogged: false,
+              token: '',
+              rightMode: false,
+            },
+            getters: {
+              isInCart: () => true,
+              getRateByProductId: () => 3,
+            },
+          },
+        },
+      },
+    });
+    // Assert the rendered text of the component
+    expect(wrapper.text()).toContain('');
+  });
+});
