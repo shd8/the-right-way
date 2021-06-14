a3c12567f38997c15160928b17166a3f98dc3ad0
Fernando Gomez <shadyfast8.services@gmail.com>
2021-06-15 00:20:16 +0200

    Test Detail up to 100%

--- e-commerce/frontend/tests/unit/detail.spec.ts
+++ e-commerce/frontend/tests/unit/detail.spec.ts
@@ -1,47 +1,103 @@
 import { mount } from '@vue/test-utils';
 import Detail from '@/views/Detail.vue';
+import router from '@/router/index';
 
 test('displays message', () => {
-  const mockedResult = {
-    name: 'product1',
-    images: [],
-    price: 1,
-    stock: 1,
-    rating: {
-      user: 'admin',
-      rating: 1,
-      comment: 'sudo',
-    },
-  };
-
   const wrapper = mount(Detail, {
     global: {
+      plugins: [router],
       mocks: {
-        methods: {
-          handleToggleModal: jest.fn(),
-        },
         $store: {
           state: {
+            wishlist: ['abcd123'],
+            cart: ['abcd123', 'abcd321'],
+            products: [
+              {
+                _id: 'abcd123',
+                name: 'product1',
+                category: 'LEFTICIANS',
+                price: 5,
+                stock: 0,
+                images: [],
+                ratings: [
+                  {
+                    user: 'one',
+                    rating: 1,
+                    comment: 'comment one',
+                  },
+                  {
+                    user: 'two',
+                    rating: 2,
+                    comment: 'comment two',
+                  },
+                ],
+              },
+              {
+                _id: 'abcd321',
+                name: 'product2',
+                category: 'OFFICE',
+                price: 0,
+                stock: 0,
+                images: [],
+                ratings: [],
+              },
+            ],
             currentProduct: {
-              name: 'product1',
+              _id: '',
+              name: 'productA',
+              category: '',
+              price: 0,
+              stock: 0,
               images: [],
-              price: 1,
-              stock: 1,
-              rating: {
-                user: 'admin',
+              ratings: [{
+                user: 'one',
                 rating: 1,
-                comment: 'sudo',
+                comment: 'comment one',
+              },
+              {
+                user: 'two',
+                rating: 2,
+                comment: 'comment two',
+              },
+              ],
+            },
+            currentUser: {
+              _id: '',
+              username: '',
+              email: '',
+              password: '',
+              address: {
+                country: '',
+                city: '',
+                street: '',
+                postalCode: '',
               },
             },
+            isUserLogged: true,
+            token: '',
+            rightMode: false,
           },
           getters: {
-            getProductById: () => mockedResult,
-            getCurrentProductRate: 5,
-            isInWishlist: true,
+            isInWishlist: () => true,
+            isInCart: () => true,
+            isUserLogged: () => true,
+            getProductById: () => '',
+            getCurrentProductRate: 2,
+            getProductsByCategory: () => [
+              {
+                _id: 1,
+                name: 'admin',
+                price: 1,
+                images: [],
+                category: 'LEFTOLOGY',
+              },
+            ],
           },
           actions: {
-            fetchProductsFromApi: {},
+            fetchProductFromApi: jest.fn(),
           },
+          dispatch: jest.fn(),
+          commit: jest.fn(),
         },
       },
     },
@@ -51,5 +107,5 @@ test('displays message', () => {
   });
 
   // Assert the rendered text of the component
-  expect(wrapper.text()).toContain('We have 1 in stock!');
+  expect(wrapper.text()).toContain('productA$ 02We have 0 in stock!one1comment onetwo2comment two');
 });
--- e-commerce/frontend/tests/unit/wishlist.spec.ts
+++
@@ -1,9 +0,0 @@
-// import { mount } from '@vue/test-utils';
-// import Wishlist from '@/views/Wishlist.vue';
-
-// test('displays message', () => {
-//   const wrapper = mount(Wishlist);
-
-//   // Assert the rendered text of the component
-//   expect(wrapper.text()).toContain('Wishlist works');
-// });
