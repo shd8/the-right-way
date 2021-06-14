86c12b63c410710a06cfea5821d872f2368eaeea
Fernando Gomez <shadyfast8.services@gmail.com>
2021-06-15 00:06:01 +0200

    Test profile up to 100%

--- e-commerce/frontend/tests/unit/profile.spec.ts
+++ e-commerce/frontend/tests/unit/profile.spec.ts
@@ -1,9 +1,114 @@
-// import { mount } from '@vue/test-utils';
-// import Profile from '@/views/Profile.vue';
+import { mount } from '@vue/test-utils';
+import Profile from '@/views/Profile.vue';
+import router from '@/router/index';
 
-// test('displays message', () => {
-//   const wrapper = mount(Profile);
+jest.mock('@/store/actions');
 
-//   // Assert the rendered text of the component
-//   expect(wrapper.text()).toContain('Profile works');
-// });
+const mockRouter = {
+  push: jest.fn(),
+};
+
+describe('Given a Dashboard component', () => {
+  test('Should mount a Dashboard View', () => {
+    window.scrollTo = () => null;
+
+    const wrapper = mount(Profile, {
+      props: {
+        id: '1',
+        name: 'Product1',
+        price: 3,
+        images: [],
+      },
+      global: {
+        mocks: {
+          $router: mockRouter,
+          $store: {
+            state: {
+              wishlist: ['abcd123'],
+              cart: ['abcd123', 'abcd321'],
+              products: [
+                {
+                  _id: 'abcd123',
+                  name: 'product1',
+                  category: 'LEFTICIANS',
+                  price: 5,
+                  stock: 0,
+                  images: [],
+                  ratings: [
+                    {
+                      user: 'one',
+                      rating: 1,
+                      comment: 'comment one',
+                    },
+                    {
+                      user: 'two',
+                      rating: 2,
+                      comment: 'comment two',
+                    },
+                  ],
+                },
+                {
+                  _id: 'abcd321',
+                  name: 'product2',
+                  category: 'OFFICE',
+                  price: 0,
+                  stock: 0,
+                  images: [],
+                  ratings: [],
+                },
+              ],
+              currentProduct: {
+                _id: '',
+                name: 'productA',
+                category: '',
+                price: 0,
+                stock: 0,
+                images: [],
+                ratings: [{
+                  user: 'one',
+                  rating: 1,
+                  comment: 'comment one',
+                },
+                {
+                  user: 'two',
+                  rating: 2,
+                  comment: 'comment two',
+                },
+                ],
+              },
+              currentUser: {
+                _id: '',
+                username: '',
+                email: '',
+                password: '',
+                address: {
+                  country: '',
+                  city: '',
+                  street: '',
+                  postalCode: '',
+                },
+              },
+              isUserLogged: true,
+              token: '',
+              rightMode: false,
+            },
+            getters: {
+              isInCart: () => true,
+              getProductById: () => 'abc123',
+              getCartPrice: () => 3,
+              getCartLength: () => 2,
+              getWishlistLength: () => 2,
+            },
+            dispatch: jest.fn(),
+            commit: jest.fn(),
+          },
+        },
+      },
+    });
+
+    wrapper.find('router-link').trigger('click');
+
+    // Assert the rendered text of the component
+    expect(wrapper.text()).toContain('');
+  });
+});
