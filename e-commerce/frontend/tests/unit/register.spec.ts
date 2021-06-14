2dcb71fb0c0c4875595e68ec8ba59039fa785a05
Fernando Gomez <shadyfast8.services@gmail.com>
2021-06-14 19:13:03 +0200

    Test Register component up to 91%

--- e-commerce/frontend/tests/unit/register.spec.ts
+++ e-commerce/frontend/tests/unit/register.spec.ts
@@ -1,8 +1,9 @@
 import { mount } from '@vue/test-utils';
 import Register from '@/components/Register.vue';
+import router from '@/router/index';
 
 describe('Given a register component', () => {
-  test('Mount a Register component', () => {
+  test('Click register with all data', () => {
     const wrapper = mount(Register, {
       global: {
         mocks: {
@@ -94,7 +95,105 @@ describe('Given a register component', () => {
       },
     });
 
+    const email = wrapper.findAll('input')[0];
+    const password = wrapper.findAll('input')[1];
+
+    email.element.value = 'a@mail.com';
+    password.element.value = '1234';
+
+    wrapper.find('form').trigger('submit');
+
     // Assert the rendered text of the component
     expect(wrapper.text()).toContain('Fill in all the fields and click on register Please, check the following errors: Credentials  Address');
+  });
+
+  test('Mount a Register component when user is logged', () => {
+    const wrapper = mount(Register, {
+      global: {
+        plugins: [router],
+        mocks: {
+          $store: {
+            state: {
+              wishlist: ['abcd123'],
+              cart: ['abcd123', 'abcd321'],
+              products: [
+                {
+                  _id: 'abcd123',
+                  name: 'product1',
+                  category: 'LEFTICIANS',
+                  price: 5,
+                  stock: 0,
+                  images: [],
+                  ratings: [
+                    {
+                      user: 'one',
+                      rating: 1,
+                      comment: 'comment one',
+                    },
+                    {
+                      user: 'two',
+                      rating: 2,
+                      comment: 'comment two',
+                    },
+                  ],
+                },
+                {
+                  _id: 'abcd321',
+                  name: 'product2',
+                  category: 'OFFICE',
+                  price: 0,
+                  stock: 0,
+                  images: [],
+                  ratings: [],
+                },
+              ],
+              currentProduct: {
+                _id: '',
+                name: 'productA',
+                category: '',
+                price: 0,
+                stock: 0,
+                images: [],
+                ratings: [{
+                  user: 'one',
+                  rating: 1,
+                  comment: 'comment one',
+                },
+                {
+                  user: 'two',
+                  rating: 2,
+                  comment: 'comment two',
+                },
+                ],
+              },
+              currentUser: {
+                _id: '',
+                username: '',
+                email: '',
+                password: '',
+                address: {
+                  country: '',
+                  city: '',
+                  street: '',
+                  postalCode: '',
+                },
+              },
+              isUserLogged: true,
+              token: '',
+              rightMode: false,
+            },
+            getters: {
+              isUserLogged: () => true,
+            },
+            actions: {
+              logUserRequest: () => 1,
+            },
+          },
+        },
+      },
+    });
+
+    // Assert the rendered text of the component
+    expect(wrapper.text()).toContain('');
   });
 });
