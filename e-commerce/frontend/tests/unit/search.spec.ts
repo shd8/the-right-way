561383f8d10fe7373065c44890d148c5fd5719f9
Fernando Gomez <shadyfast8.services@gmail.com>
2021-06-15 00:12:05 +0200

    Delete error from search component test

--- e-commerce/frontend/tests/unit/search.spec.ts
+++ e-commerce/frontend/tests/unit/search.spec.ts
@@ -8,29 +8,94 @@ test('displays message', () => {
       plugins: [router],
       mocks: {
         $store: {
-          disptach: jest.fn(),
           state: {
+            wishlist: ['abcd123'],
+            cart: ['abcd123', 'abcd321'],
             products: [
+              {
+                _id: 'abcd123',
+                name: 'product1',
+                category: 'LEFTICIANS',
+                price: 5,
+                stock: 0,
+                images: [],
+                ratings: [
+                  {
+                    user: 'one',
+                    rating: 1,
+                    comment: 'comment one',
+                  },
+                  {
+                    user: 'two',
+                    rating: 2,
+                    comment: 'comment two',
+                  },
+                ],
+              },
+              {
+                _id: 'abcd321',
+                name: 'product2',
+                category: 'OFFICE',
+                price: 0,
+                stock: 0,
+                images: [],
+                ratings: [],
+              },
+            ],
+            currentProduct: {
+              _id: '',
+              name: 'productA',
+              category: '',
+              price: 0,
+              stock: 0,
+              images: [],
+              ratings: [{
+                user: 'one',
+                rating: 1,
+                comment: 'comment one',
+              },
+              {
+                user: 'two',
+                rating: 2,
+                comment: 'comment two',
+              },
+              ],
+            },
+            currentUser: {
+              _id: '',
+              username: '',
+              email: '',
+              password: '',
+              address: {
+                country: '',
+                city: '',
+                street: '',
+                postalCode: '',
+              },
+            },
+            isUserLogged: true,
+            token: '',
+            rightMode: false,
+          },
+          getters: {
+            isInWishlist: () => true,
+            isInCart: () => true,
+            isUserLogged: () => true,
+            getProductsByCategory: () => [
               {
                 _id: 1,
                 name: 'admin',
                 price: 1,
                 images: [],
+                category: 'LEFTOLOGY',
               },
             ],
           },
-          getters: {
-            isInWishlist: true,
-          },
           actions: {
-            fetchProductsFromApi: jest.fn(),
+            fetchProductFromApi: jest.fn(),
           },
-        },
-        methods: {
-          mapActions: jest.fn(),
-        },
-        mounted: {
-          fetchProductsFromApi: jest.fn(),
+          dispatch: jest.fn(),
+          commit: jest.fn(),
         },
       },
     },
@@ -40,5 +105,5 @@ test('displays message', () => {
   });
 
   // Assert the rendered text of the component
-  expect(wrapper.text()).toContain('We have 1 in stock!');
+  expect(wrapper.text()).toContain('All our productsproduct1$ 5product2$ 0');
 });
