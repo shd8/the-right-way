2cb56c6ec465cc148c5b78a9a3dd121f9ec90581
Fernando Gomez <shadyfast8.services@gmail.com>
2021-06-14 23:53:43 +0200

    Test Cart view up to 100%

---
+++ e-commerce/frontend/tests/unit/cartView.spec.ts
@@ -0,0 +1,108 @@
+import { mount } from '@vue/test-utils';
+import Cart from '@/views/Cart.vue';
+import router from '@/router/index';
+
+jest.mock('@/store/actions');
+
+describe('Given a Wishlist Product component', () => {
+  test('Should mount Wishlist Product', async () => {
+    window.scrollTo = () => null;
+
+    const wrapper = mount(Cart, {
+      props: {
+        id: '1',
+        name: 'Product1',
+        price: 3,
+        images: [],
+      },
+      global: {
+        plugins: [router],
+        mocks: {
+          $store: {
+            state: {
+              wishlist: ['abcd123'],
+              cart: ['abcd123', 'abcd321'],
+              products: [
+                {
+                  _id: 'abcd123',
+                  name: 'product1',
+                  category: 'LEFTICIANS',
+                  price: 5,
+                  stock: 0,
+                  images: [],
+                  ratings: [
+                    {
+                      user: 'one',
+                      rating: 1,
+                      comment: 'comment one',
+                    },
+                    {
+                      user: 'two',
+                      rating: 2,
+                      comment: 'comment two',
+                    },
+                  ],
+                },
+                {
+                  _id: 'abcd321',
+                  name: 'product2',
+                  category: 'OFFICE',
+                  price: 0,
+                  stock: 0,
+                  images: [],
+                  ratings: [],
+                },
+              ],
+              currentProduct: {
+                _id: '',
+                name: 'productA',
+                category: '',
+                price: 0,
+                stock: 0,
+                images: [],
+                ratings: [{
+                  user: 'one',
+                  rating: 1,
+                  comment: 'comment one',
+                },
+                {
+                  user: 'two',
+                  rating: 2,
+                  comment: 'comment two',
+                },
+                ],
+              },
+              currentUser: {
+                _id: '',
+                username: '',
+                email: '',
+                password: '',
+                address: {
+                  country: '',
+                  city: '',
+                  street: '',
+                  postalCode: '',
+                },
+              },
+              isUserLogged: true,
+              token: '',
+              rightMode: false,
+            },
+            getters: {
+              isInCart: () => true,
+              getProductById: () => 'abc123',
+              getCartPrice: () => 3,
+            },
+            dispatch: jest.fn(),
+            commit: jest.fn(),
+          },
+        },
+      },
+    });
+
+    wrapper.find('.purchase__button').trigger('click');
+
+    // Assert the rendered text of the component
+    expect(wrapper.text()).toContain('');
+  });
+});
