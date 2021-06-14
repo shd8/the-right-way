9f55f9372e1c8aa94c4fa92beb0eba923c007e16
Fernando Gomez <shadyfast8.services@gmail.com>
2021-06-14 18:38:30 +0200

    Test login up to 80%

--- e-commerce/frontend/tests/unit/login.spec.ts
+++ e-commerce/frontend/tests/unit/login.spec.ts
@@ -3,7 +3,7 @@ import Login from '@/components/Login.vue';
 import router from '@/router/index';
 
 describe('Given a login component', () => {
-  test('Mount a Login component', () => {
+  test('Click login', async () => {
     const wrapper = mount(Login, {
       global: {
         mocks: {
@@ -95,11 +95,117 @@ describe('Given a login component', () => {
       },
     });
 
+    await wrapper.find('form').trigger('submit');
+
     // Assert the rendered text of the component
     expect(wrapper.text()).toContain('Login or create an account to buy stuff, save your Wishlist and your conversations !');
   });
 
-  test('Mount a Login component', () => {
+  test('Click login with password and email', () => {
+    const wrapper = mount(Login, {
+      global: {
+        mocks: {
+          methods: {
+            handleToggleModal: jest.fn(),
+          },
+          $store: {
+            state: {
+              wishlist: ['abcd123'],
+              cart: ['abcd123', 'abcd321'],
+              products: [
+                {
+                  _id: 'abcd123',
+                  name: 'product1',
+                  category: 'LEFTICIANS',
+                  price: 5,
+                  stock: 0,
+                  images: [],
+                  ratings: [
+                    {
+                      user: 'one',
+                      rating: 1,
+                      comment: 'comment one',
+                    },
+                    {
+                      user: 'two',
+                      rating: 2,
+                      comment: 'comment two',
+                    },
+                  ],
+                },
+                {
+                  _id: 'abcd321',
+                  name: 'product2',
+                  category: 'OFFICE',
+                  price: 0,
+                  stock: 0,
+                  images: [],
+                  ratings: [],
+                },
+              ],
+              currentProduct: {
+                _id: '',
+                name: 'productA',
+                category: '',
+                price: 0,
+                stock: 0,
+                images: [],
+                ratings: [{
+                  user: 'one',
+                  rating: 1,
+                  comment: 'comment one',
+                },
+                {
+                  user: 'two',
+                  rating: 2,
+                  comment: 'comment two',
+                },
+                ],
+              },
+              currentUser: {
+                _id: '',
+                username: '',
+                email: '',
+                password: '',
+                address: {
+                  country: '',
+                  city: '',
+                  street: '',
+                  postalCode: '',
+                },
+              },
+              isUserLogged: false,
+              token: '',
+              rightMode: false,
+            },
+            getters: {
+              getCurrentProductRate: 5,
+              isInWishlist: true,
+            },
+            actions: {
+              fetchProductsFromApi: {},
+            },
+          },
+        },
+      },
+      props: {
+        id: 2,
+      },
+    });
+
+    const email = wrapper.findAll('input')[0];
+    const password = wrapper.findAll('input')[1];
+
+    email.element.value = 'a@mail.com';
+    password.element.value = '1234';
+
+    wrapper.find('form').trigger('submit');
+
+    // Assert the rendered text of the component
+    expect(wrapper.text()).toContain('Login or create an account to buy stuff, save your Wishlist and your conversations !');
+  });
+
+  test('Mount a Login component when user is logged', () => {
     const wrapper = mount(Login, {
       global: {
         plugins: [router],
