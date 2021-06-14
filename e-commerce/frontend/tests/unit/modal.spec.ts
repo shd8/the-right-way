e4b09f93ab588a884a71ef51d6aa8984afe06f05
Fernando Gomez <shadyfast8.services@gmail.com>
2021-06-14 18:53:29 +0200

    Test Modal component up to 89%

--- e-commerce/frontend/tests/unit/modal.spec.ts
+++ e-commerce/frontend/tests/unit/modal.spec.ts
@@ -97,4 +97,103 @@ describe('Given a Modal component', () => {
     // Assert the rendered text of the component
     expect(wrapper.text()).toContain('');
   });
+
+  test('Click on modal button', async () => {
+    const wrapper = mount(Modal, {
+      global: {
+        mocks: {
+          methods: {
+            handleToggleModal: jest.fn(),
+          },
+          $store: {
+            state: {
+              wishlist: ['abcd123'],
+              cart: ['abcd123', 'abcd321'],
+              products: [
+                {
+                  _id: 'abcd123',
+                  name: 'product1',
+                  category: 'LEFTICIANS',
+                  price: 5,
+                  stock: 0,
+                  images: [],
+                  ratings: [
+                    {
+                      user: 'one',
+                      rating: 1,
+                      comment: 'comment one',
+                    },
+                    {
+                      user: 'two',
+                      rating: 2,
+                      comment: 'comment two',
+                    },
+                  ],
+                },
+                {
+                  _id: 'abcd321',
+                  name: 'product2',
+                  category: 'OFFICE',
+                  price: 0,
+                  stock: 0,
+                  images: [],
+                  ratings: [],
+                },
+              ],
+              currentProduct: {
+                _id: '',
+                name: 'productA',
+                category: '',
+                price: 0,
+                stock: 0,
+                images: [],
+                ratings: [{
+                  user: 'one',
+                  rating: 1,
+                  comment: 'comment one',
+                },
+                {
+                  user: 'two',
+                  rating: 2,
+                  comment: 'comment two',
+                },
+                ],
+              },
+              currentUser: {
+                _id: '',
+                username: '',
+                email: '',
+                password: '',
+                address: {
+                  country: '',
+                  city: '',
+                  street: '',
+                  postalCode: '',
+                },
+              },
+              isUserLogged: true,
+              token: '',
+              rightMode: false,
+            },
+            getters: {
+              getCurrentProductRate: 5,
+              isInWishlist: true,
+            },
+            actions: {
+              fetchProductsFromApi: {},
+            },
+          },
+        },
+      },
+      props: {
+        isModalOpen: true,
+      },
+    });
+
+    await wrapper.find('.modal__header-button').trigger('click');
+
+    await wrapper.unmount();
+    // Assert the rendered text of the component
+    expect(wrapper.text()).toContain('');
+  });
 });
