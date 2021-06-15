5087d4544387f722fc8f11563a01d6876cac4a14
Fernando Gomez <shadyfast8.services@gmail.com>
2021-06-15 10:37:35 +0200

    Test actions up to 100%

---
+++ e-commerce/frontend/tests/test-utils.ts
@@ -0,0 +1,24 @@
+import { State } from '@/types/interfaces';
+import { Commit } from 'vuex';
+
+const configActionContextAndState = (commit: Commit, state: State): any => ({
+  commit,
+  state,
+  dispatch: jest.fn(),
+  getters: jest.fn(),
+  rootState: {} as State,
+  rootGetters: jest.fn(),
+});
+
+const configActionContext = (commit: Commit): any => ({
+  commit,
+  dispatch: jest.fn(),
+  getters: jest.fn(),
+  rootState: {} as State,
+  rootGetters: jest.fn(),
+});
+
+export {
+  configActionContext,
+  configActionContextAndState,
+};
--- e-commerce/frontend/tests/unit/actions.spec.ts
+++ e-commerce/frontend/tests/unit/actions.spec.ts
@@ -1,11 +1,18 @@
 import actions from '@/store/actions';
+import { Commit } from 'vuex';
+import { State, User } from '@/types/interfaces';
+import axios from 'axios';
+import { configActionContextAndState, configActionContext } from '../test-utils';
 
-jest.mock('@/store/actions');
+jest.mock('axios');
+const mockedAxios = axios as jest.Mocked<typeof axios>;
+const commit = jest.fn() as jest. MockedFunction<Commit>;
 
-describe('actions', () => {
-  test('getAllProducts', () => {
-    const commit = spyOn;
-    const state = {
+describe('Given an object of actions', () => {
+  let state: State;
+
+  beforeEach(() => {
+    state = {
       wishlist: ['abcd123'],
       cart: ['abcd123', 'abcd321'],
       products: [
@@ -74,14 +81,91 @@ describe('actions', () => {
       token: '',
       rightMode: true,
     };
-
-    // console.log(actions.retrieveFromUserCart);
-
-    // actions.retrieveFromUserCart({ commit, state }, 'abc');
-
-    // expect(commit.args).to.deep.equal([
-    //   ['REQUEST_PRODUCTS'],
-    //   ['RECEIVE_PRODUCTS', { /* mocked response */ }],
-    // ]);
   });
+
+  test('Fetch all products from API', async () => {
+    mockedAxios.get.mockResolvedValue({
+      data: [],
+    });
+
+    await actions.fetchProductsFromApi(configActionContext(commit));
+
+    expect(commit).toHaveBeenCalled();
+  });
+
+  test('Fetch a product from API', async () => {
+    mockedAxios.get.mockResolvedValue({
+      data: [],
+    });
+
+    await actions.fetchProductFromApi(configActionContext(commit), 'abc');
+
+    expect(commit).toHaveBeenCalled();
+  });
+
+  test('Log a user', async () => {
+    mockedAxios.post.mockResolvedValue({
+      data: [],
+    });
+
+    const userData = {} as User;
+
+    await actions.logUserRequest(configActionContext(commit), userData);
+
+    expect(commit).toHaveBeenCalled();
+  });
+
+  test('Register a new user successfully', async () => {
+    mockedAxios.post.mockResolvedValue({
+      data: [],
+    });
+
+    const userData = {} as User;
+
+    await actions.registerUserRequest(configActionContext(commit), userData);
+
+    expect(axios.post).toHaveBeenCalled();
+  });
+
+  test('Make a purchase request', async () => {
+    await actions.purchaseRequest(configActionContextAndState(commit, state));
+
+    expect(commit).toHaveBeenCalled();
+  });
+
+  test('Add to user wishlist', async () => {
+    await actions.addToUserWishlist(configActionContextAndState(commit, state), 'abc123');
+
+    expect(commit).toHaveBeenCalled();
+  });
+
+  test('Retrieve from user wishlist', async () => {
+    await actions.retrieveFromUserWishlist(configActionContextAndState(commit, state), 'abc123');
+
+    expect(commit).toHaveBeenCalled();
+  });
+
+  test('Add to user cart', async () => {
+    await actions.addToUserCart(configActionContextAndState(commit, state), 'abc123');
+
+    expect(commit).toHaveBeenCalled();
+  });
+
+  test('Retrieve from user cart', async () => {
+    await actions.retrieveFromUserCart(configActionContextAndState(commit, state), 'abc123');
+
+    expect(commit).toHaveBeenCalled();
+  });
+});
+
+test('Register a new user and catch an error', async () => {
+  mockedAxios.post.mockRejectedValue({
+    data: [],
+  });
+
+  const userData = {} as User;
+
+  await actions.registerUserRequest(configActionContext(commit), userData);
+
+  expect(axios.post).toHaveBeenCalled();
 });
