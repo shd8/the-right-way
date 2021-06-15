6e45ce193f488fb516b958a08ab53e58ea1529fe
Fernando Gomez <shadyfast8.services@gmail.com>
2021-06-15 13:47:18 +0200

    Tes login component up to cd /Users/shd8/yummy/Fer_Final-Project-202104/e-commerce/frontend

--- e-commerce/frontend/tests/unit/login.spec.ts
+++ e-commerce/frontend/tests/unit/login.spec.ts
@@ -294,4 +294,110 @@ describe('Given a login component', () => {
     // Assert the rendered text of the component
     expect(wrapper.text()).toContain('');
   });
+
+  test('Click login with user and password', async () => {
+    const wrapper = mount(Login, {
+      global: {
+        mocks: {
+          methods: {
+            handleToggleModal: jest.fn(),
+          },
+          $store: {
+            state: {
+              wishlist: ['abcd123'],
+              cart: ['abcd123', 'abcd321'],
+              products: [
+                {
+                  _id: 'abcd123',
+                  name: 'product1',
+                  category: 'LEFTICIANS',
+                  price: 5,
+                  stock: 0,
+                  images: [],
+                  ratings: [
+                    {
+                      user: 'one',
+                      rating: 1,
+                      comment: 'comment one',
+                    },
+                    {
+                      user: 'two',
+                      rating: 2,
+                      comment: 'comment two',
+                    },
+                  ],
+                },
+                {
+                  _id: 'abcd321',
+                  name: 'product2',
+                  category: 'OFFICE',
+                  price: 0,
+                  stock: 0,
+                  images: [],
+                  ratings: [],
+                },
+              ],
+              currentProduct: {
+                _id: '',
+                name: 'productA',
+                category: '',
+                price: 0,
+                stock: 0,
+                images: [],
+                ratings: [{
+                  user: 'one',
+                  rating: 1,
+                  comment: 'comment one',
+                },
+                {
+                  user: 'two',
+                  rating: 2,
+                  comment: 'comment two',
+                },
+                ],
+              },
+              currentUser: {
+                _id: '',
+                username: '',
+                email: '',
+                password: '',
+                address: {
+                  country: '',
+                  city: '',
+                  street: '',
+                  postalCode: '',
+                },
+              },
+              isUserLogged: false,
+              token: '',
+              rightMode: false,
+            },
+            getters: {
+              getCurrentProductRate: 5,
+              isInWishlist: true,
+            },
+            actions: {
+              logUserRequest: jest.fn(),
+            },
+            dispatch: jest.fn(),
+            commit: jest.fn(),
+          },
+        },
+      },
+      props: {
+        id: 2,
+      },
+      data() {
+        return {
+          email: 'a@mail.com',
+          password: '1234',
+        };
+      },
+    });
+
+    await wrapper.find('form').trigger('submit');
+
+    // Assert the rendered text of the component
+    expect(wrapper.text()).toContain('Login or create an account to buy stuff, save your Wishlist and your conversations !');
+  });
 });
