1d6611cafde35c315138d19df656723ac0202a37
Fernando Gomez <shadyfast8.services@gmail.com>
2021-06-15 13:53:52 +0200

    Test register up to 91%

--- e-commerce/frontend/tests/unit/register.spec.ts
+++ e-commerce/frontend/tests/unit/register.spec.ts
@@ -3,7 +3,7 @@ import Register from '@/components/Register.vue';
 import router from '@/router/index';
 
 describe('Given a register component', () => {
-  test('Click register with all data', () => {
+  test('Click register without data', () => {
     const wrapper = mount(Register, {
       global: {
         mocks: {
@@ -92,6 +92,122 @@ describe('Given a register component', () => {
       },
       props: {
         id: 2,
+      },
+    });
+
+    const email = wrapper.findAll('input')[0];
+    const password = wrapper.findAll('input')[1];
+
+    email.element.value = 'a@mail.com';
+    password.element.value = '1234';
+
+    wrapper.find('form').trigger('submit');
+
+    // Assert the rendered text of the component
+    expect(wrapper.text()).toContain('Fill in all the fields and click on register Please, check the following errors: Credentials  Address');
+  });
+
+  test('Click register with all data', () => {
+    const wrapper = mount(Register, {
+      global: {
+        mocks: {
+          methods: {
+            handleToggleModal: jest.fn(),
+          },
+          $store: {
+            state: {
+              wishlist: ['abcd123'],
+              cart: ['abcd123', 'abcd321'],
+              products: [
+                {
+                  _id: 'abcd123',
+                  name: 'product1',
+                  category: 'LEFTICIANS',
+                  price: 5,
+                  stock: 0,
+                  images: [],
+                  ratings: [
+                    {
+                      user: 'one',
+                      rating: 1,
+                      comment: 'comment one',
+                    },
+                    {
+                      user: 'two',
+                      rating: 2,
+                      comment: 'comment two',
+                    },
+                  ],
+                },
+                {
+                  _id: 'abcd321',
+                  name: 'product2',
+                  category: 'OFFICE',
+                  price: 0,
+                  stock: 0,
+                  images: [],
+                  ratings: [],
+                },
+              ],
+              currentProduct: {
+                _id: '',
+                name: 'productA',
+                category: '',
+                price: 0,
+                stock: 0,
+                images: [],
+                ratings: [{
+                  user: 'one',
+                  rating: 1,
+                  comment: 'comment one',
+                },
+                {
+                  user: 'two',
+                  rating: 2,
+                  comment: 'comment two',
+                },
+                ],
+              },
+              currentUser: {
+                _id: '',
+                username: '',
+                email: '',
+                password: '',
+                address: {
+                  country: '',
+                  city: '',
+                  street: '',
+                  postalCode: '',
+                },
+              },
+              isUserLogged: false,
+              token: '',
+              rightMode: false,
+            },
+            getters: {
+              getCurrentProductRate: 5,
+              isInWishlist: true,
+            },
+            actions: {
+              fetchProductsFromApi: {},
+              registerUserRequest: jest.fn(),
+            },
+          },
+        },
+      },
+      props: {
+        id: 2,
+      },
+      data() {
+        return {
+          username: 'a',
+          email: 'a@mail.com',
+          password: '1234',
+          country: 'a',
+          city: 'a',
+          street: 'a',
+          postalCode: '',
+        };
       },
     });
 
