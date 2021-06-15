2696fe214e68f6c72949384af595fdfdb56284a2
Fernando Gomez <shadyfast8.services@gmail.com>
2021-06-15 18:16:42 +0200

    Delete duplicate blocks of code from category selector component

--- e-commerce/frontend/tests/unit/categorySelector.spec.ts
+++ e-commerce/frontend/tests/unit/categorySelector.spec.ts
@@ -1,6 +1,7 @@
 import { mount } from '@vue/test-utils';
 import CategorySelector from '@/components/CategorySelector.vue';
 import router from '@/router/index';
+import state from '../mocked-state';
 
 const mockRouter = {
   push: jest.fn(),
@@ -16,75 +17,7 @@ describe('Given a category selector', () => {
             push: jest.fn(),
           },
           $store: {
-            state: {
-              wishlist: ['abcd123'],
-              cart: ['abcd123', 'abcd321'],
-              products: [
-                {
-                  _id: 'abcd123',
-                  name: 'product1',
-                  category: 'LEFTICIANS',
-                  price: 5,
-                  stock: 0,
-                  images: [],
-                  ratings: [
-                    {
-                      user: 'one',
-                      rating: 1,
-                      comment: 'comment one',
-                    },
-                    {
-                      user: 'two',
-                      rating: 2,
-                      comment: 'comment two',
-                    },
-                  ],
-                },
-                {
-                  _id: 'abcd321',
-                  name: 'product2',
-                  category: 'OFFICE',
-                  price: 0,
-                  stock: 0,
-                  images: [],
-                  ratings: [],
-                },
-              ],
-              currentProduct: {
-                _id: '',
-                name: 'productA',
-                category: '',
-                price: 0,
-                stock: 0,
-                images: [],
-                ratings: [{
-                  user: 'one',
-                  rating: 1,
-                  comment: 'comment one',
-                },
-                {
-                  user: 'two',
-                  rating: 2,
-                  comment: 'comment two',
-                },
-                ],
-              },
-              currentUser: {
-                _id: '',
-                username: '',
-                email: '',
-                password: '',
-                address: {
-                  country: '',
-                  city: '',
-                  street: '',
-                  postalCode: '',
-                },
-              },
-              isUserLogged: false,
-              token: '',
-              rightMode: false,
-            },
+            state,
             getters: {
               isInCart: () => true,
             },
@@ -103,73 +36,8 @@ describe('Given a category selector', () => {
 
     const $store = {
       state: {
-        wishlist: ['abcd123'],
-        cart: ['abcd123', 'abcd321'],
-        products: [
-          {
-            _id: 'abcd123',
-            name: 'product1',
-            category: 'LEFTICIANS',
-            price: 5,
-            stock: 0,
-            images: [],
-            ratings: [
-              {
-                user: 'one',
-                rating: 1,
-                comment: 'comment one',
-              },
-              {
-                user: 'two',
-                rating: 2,
-                comment: 'comment two',
-              },
-            ],
-          },
-          {
-            _id: 'abcd321',
-            name: 'product2',
-            category: 'OFFICE',
-            price: 0,
-            stock: 0,
-            images: [],
-            ratings: [],
-          },
-        ],
-        currentProduct: {
-          _id: '',
-          name: 'productA',
-          category: '',
-          price: 0,
-          stock: 0,
-          images: [],
-          ratings: [{
-            user: 'one',
-            rating: 1,
-            comment: 'comment one',
-          },
-          {
-            user: 'two',
-            rating: 2,
-            comment: 'comment two',
-          },
-          ],
-        },
-        currentUser: {
-          _id: '',
-          username: '',
-          email: '',
-          password: '',
-          address: {
-            country: '',
-            city: '',
-            street: '',
-            postalCode: '',
-          },
-        },
+        ...state,
         isUserLogged: true,
-        token: '',
-        rightMode: true,
       },
       getters: {
         isInWishlist: () => true,
