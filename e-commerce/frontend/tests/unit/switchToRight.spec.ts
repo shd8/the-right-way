c80a4822129afbc4bd566060063a3fc95e03b2ad
Fernando Gomez <shadyfast8.services@gmail.com>
2021-06-15 18:57:40 +0200

    Delete duplicate blocks from tests

--- e-commerce/frontend/tests/unit/switchToRight.spec.ts
+++ e-commerce/frontend/tests/unit/switchToRight.spec.ts
@@ -1,5 +1,6 @@
 import { mount } from '@vue/test-utils';
 import SwitchToRight from '@/components/SwitchToRight.vue';
+import state from '../mocked-state';
 
 describe('Given switch to right component', () => {
   test('Should mount a switch to right component', () => {
@@ -8,73 +9,8 @@ describe('Given switch to right component', () => {
         mocks: {
           $store: {
             state: {
-              wishlist: ['abcd123'],
-              cart: ['abcd123', 'abcd321'],
-              products: [
-                {
-                  _id: 'abcd123',
-                  name: 'product1',
-                  category: 'LEFTICIANS',
-                  price: 5,
-                  stock: 0,
-                  images: [],
-                  ratings: [
-                    {
-                      user: 'one',
-                      rating: 1,
-                      comment: 'comment one',
-                    },
-                    {
-                      user: 'two',
-                      rating: 2,
-                      comment: 'comment two',
-                    },
-                  ],
-                },
-                {
-                  _id: 'abcd321',
-                  name: 'product2',
-                  category: 'OFFICE',
-                  price: 0,
-                  stock: 0,
-                  images: [],
-                  ratings: [],
-                },
-              ],
-              currentProduct: {
-                _id: '',
-                name: 'productA',
-                category: '',
-                price: 0,
-                stock: 0,
-                images: [],
-                ratings: [{
-                  user: 'one',
-                  rating: 1,
-                  comment: 'comment one',
-                },
-                {
-                  user: 'two',
-                  rating: 2,
-                  comment: 'comment two',
-                },
-                ],
-              },
-              currentUser: {
-                _id: '',
-                username: '',
-                email: '',
-                password: '',
-                address: {
-                  country: '',
-                  city: '',
-                  street: '',
-                  postalCode: '',
-                },
-              },
+              ...state,
               isUserLogged: false,
-              token: '',
-              rightMode: false,
             },
             getters: {
               isInWishlist: () => true,
