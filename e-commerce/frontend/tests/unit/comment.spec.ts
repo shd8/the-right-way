5c6237476d0e2f2a3a3059e62518e9fd506910f7
Fernando Gomez <shadyfast8.services@gmail.com>
2021-06-15 18:20:23 +0200

    Delete duplicate blocks from comment component, and dashboard and detail views

--- e-commerce/frontend/tests/unit/comment.spec.ts
+++ e-commerce/frontend/tests/unit/comment.spec.ts
@@ -1,5 +1,6 @@
 import { mount } from '@vue/test-utils';
 import Comment from '@/components/Comment.vue';
+import state from '../mocked-state';
 
 describe('Given an Comment component', () => {
   test('Should mount comment component', () => {
@@ -12,75 +13,7 @@ describe('Given an Comment component', () => {
       global: {
         mocks: {
           $store: {
-            state: {
-              wishlist: ['abcd123'],
-              cart: ['abcd123', 'abcd321'],
-              products: [
-                {
-                  _id: 'abcd123',
-                  name: 'product1',
-                  category: 'LEFTICIANS',
-                  price: 5,
-                  stock: 0,
-                  images: [],
-                  ratings: [
-                    {
-                      user: 'one',
-                      rating: 1,
-                      comment: 'comment one',
-                    },
-                    {
-                      user: 'two',
-                      rating: 2,
-                      comment: 'comment two',
-                    },
-                  ],
-                },
-                {
-                  _id: 'abcd321',
-                  name: 'product2',
-                  category: 'OFFICE',
-                  price: 0,
-                  stock: 0,
-                  images: [],
-                  ratings: [],
-                },
-              ],
-              currentProduct: {
-                _id: '',
-                name: 'productA',
-                category: '',
-                price: 0,
-                stock: 0,
-                images: [],
-                ratings: [{
-                  user: 'one',
-                  rating: 1,
-                  comment: 'comment one',
-                },
-                {
-                  user: 'two',
-                  rating: 2,
-                  comment: 'comment two',
-                },
-                ],
-              },
-              currentUser: {
-                _id: '',
-                username: '',
-                email: '',
-                password: '',
-                address: {
-                  country: '',
-                  city: '',
-                  street: '',
-                  postalCode: '',
-                },
-              },
-              isUserLogged: false,
-              token: '',
-              rightMode: false,
-            },
+            state,
             getters: {
               isInCart: () => true,
             },
--- e-commerce/frontend/tests/unit/dashboard.spec.ts
+++ e-commerce/frontend/tests/unit/dashboard.spec.ts
@@ -1,6 +1,7 @@
 import { mount } from '@vue/test-utils';
 import Dashboard from '@/views/Dashboard.vue';
 import router from '@/router/index';
+import state from '../mocked-state';
 
 jest.mock('@/store/actions');
 
@@ -20,73 +21,8 @@ describe('Given a Dashboard component', () => {
         mocks: {
           $store: {
             state: {
-              wishlist: ['abcd123'],
-              cart: ['abcd123', 'abcd321'],
-              products: [
-                {
-                  _id: 'abcd123',
-                  name: 'product1',
-                  category: 'LEFTICIANS',
-                  price: 5,
-                  stock: 0,
-                  images: [],
-                  ratings: [
-                    {
-                      user: 'one',
-                      rating: 1,
-                      comment: 'comment one',
-                    },
-                    {
-                      user: 'two',
-                      rating: 2,
-                      comment: 'comment two',
-                    },
-                  ],
-                },
-                {
-                  _id: 'abcd321',
-                  name: 'product2',
-                  category: 'OFFICE',
-                  price: 0,
-                  stock: 0,
-                  images: [],
-                  ratings: [],
-                },
-              ],
-              currentProduct: {
-                _id: '',
-                name: 'productA',
-                category: '',
-                price: 0,
-                stock: 0,
-                images: [],
-                ratings: [{
-                  user: 'one',
-                  rating: 1,
-                  comment: 'comment one',
-                },
-                {
-                  user: 'two',
-                  rating: 2,
-                  comment: 'comment two',
-                },
-                ],
-              },
-              currentUser: {
-                _id: '',
-                username: '',
-                email: '',
-                password: '',
-                address: {
-                  country: '',
-                  city: '',
-                  street: '',
-                  postalCode: '',
-                },
-              },
+              ...state,
               isUserLogged: true,
-              token: '',
-              rightMode: false,
             },
             getters: {
               isInCart: () => true,
