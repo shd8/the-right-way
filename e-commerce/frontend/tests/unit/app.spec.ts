ab1abc39776c179a1d53f52fc79ebe0cc1235c5f
Fernando Gomez <shadyfast8.services@gmail.com>
2021-06-15 18:10:14 +0200

    Delete duplicate blocks of code from app test

--- e-commerce/frontend/tests/unit/app.spec.ts
+++ e-commerce/frontend/tests/unit/app.spec.ts
@@ -1,6 +1,7 @@
 import { mount } from '@vue/test-utils';
 import App from '@/App.vue';
 import router from '@/router/index';
+import state from '../mocked-state';
 
 test('displays message', () => {
   const wrapper = mount(App, {
@@ -8,75 +9,7 @@ test('displays message', () => {
       plugins: [router],
       mocks: {
         $store: {
-          state: {
-            wishlist: ['abcd123'],
-            cart: ['abcd123', 'abcd321'],
-            products: [
-              {
-                _id: 'abcd123',
-                name: 'product1',
-                category: 'LEFTICIANS',
-                price: 5,
-                stock: 0,
-                images: [],
-                ratings: [
-                  {
-                    user: 'one',
-                    rating: 1,
-                    comment: 'comment one',
-                  },
-                  {
-                    user: 'two',
-                    rating: 2,
-                    comment: 'comment two',
-                  },
-                ],
-              },
-              {
-                _id: 'abcd321',
-                name: 'product2',
-                category: 'OFFICE',
-                price: 0,
-                stock: 0,
-                images: [],
-                ratings: [],
-              },
-            ],
-            currentProduct: {
-              _id: '',
-              name: 'productA',
-              category: '',
-              price: 0,
-              stock: 0,
-              images: [],
-              ratings: [{
-                user: 'one',
-                rating: 1,
-                comment: 'comment one',
-              },
-              {
-                user: 'two',
-                rating: 2,
-                comment: 'comment two',
-              },
-              ],
-            },
-            currentUser: {
-              _id: '',
-              username: '',
-              email: '',
-              password: '',
-              address: {
-                country: '',
-                city: '',
-                street: '',
-                postalCode: '',
-              },
-            },
-            isUserLogged: false,
-            token: '',
-            rightMode: false,
-          },
+          state,
           getters: {
             getCartLength: 2,
             getWishlistLength: 2,
