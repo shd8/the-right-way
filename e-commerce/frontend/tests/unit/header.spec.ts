d418ac596d152fd4bb8a9032e0b91d40ed75b01f
Fernando Gomez <shadyfast8.services@gmail.com>
2021-06-15 15:45:13 +0200

    Test header up to 100%

--- e-commerce/frontend/tests/unit/header.spec.ts
+++ e-commerce/frontend/tests/unit/header.spec.ts
@@ -209,4 +209,114 @@ describe('Given a header component', () => {
 
     expect(scrollToTop).toHaveBeenCalled();
   });
+
+  test('And an input', async () => {
+    window.scrollTo = () => null;
+    const $store = {
+      state: {
+        wishlist: ['abcd123'],
+        cart: ['abcd123', 'abcd321'],
+        products: [
+          {
+            _id: 'abcd123',
+            name: 'product1',
+            category: 'LEFTICIANS',
+            price: 5,
+            stock: 0,
+            images: [],
+            ratings: [
+              {
+                user: 'one',
+                rating: 1,
+                comment: 'comment one',
+              },
+              {
+                user: 'two',
+                rating: 2,
+                comment: 'comment two',
+              },
+            ],
+          },
+          {
+            _id: 'abcd321',
+            name: 'product2',
+            category: 'OFFICE',
+            price: 0,
+            stock: 0,
+            images: [],
+            ratings: [],
+          },
+        ],
+        currentProduct: {
+          _id: '',
+          name: 'productA',
+          category: '',
+          price: 0,
+          stock: 0,
+          images: [],
+          ratings: [{
+            user: 'one',
+            rating: 1,
+            comment: 'comment one',
+          },
+          {
+            user: 'two',
+            rating: 2,
+            comment: 'comment two',
+          },
+          ],
+        },
+        currentUser: {
+          _id: '',
+          username: '',
+          email: '',
+          password: '',
+          address: {
+            country: '',
+            city: '',
+            street: '',
+            postalCode: '',
+          },
+        },
+        isUserLogged: true,
+        token: '',
+        rightMode: true,
+      },
+      getters: {
+        isInCart: () => true,
+        getCartLength: () => 1,
+        getWishlistLength: () => 1,
+      },
+      dispatch: jest.fn(),
+      commit: jest.fn(),
+    };
+
+    const methods = {
+      scrollToTop: jest.fn(),
+    };
+
+    const wrapper = mount(Header, {
+      global: {
+        mocks: {
+          $router: mockRouter,
+          $store,
+          methods,
+        },
+      },
+      data() {
+        return {
+          searchInput: 'ab',
+        };
+      },
+    });
+
+    await wrapper.find('input').trigger('input');
+    const input = wrapper.find('input');
+
+    input.element.value = 'a';
+    input.element.value = 'product1';
+    input.element.value = '';
+
+    expect(wrapper.text()).toContain('() => 1() => 1');
+  });
 });
